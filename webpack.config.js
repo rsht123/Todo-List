1d80678749dc8d52b3ae927ebfcf880d95303f06
Rishit Rathod <rishitrathod@gmail.com>
2018-02-03 16:37:51 +0530

    fixed express server

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: "./src/app.js",
     output: {
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, 'public/build'),
         filename: 'bundle.js'
     },
     // devServer: {
